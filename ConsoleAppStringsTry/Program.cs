63a8f0be8761e96376c323993ce45827cd8e085a
ThibaultP-immalle <31651240+ThibaultP-immalle@users.noreply.github.com>
2018-03-27 13:15:27 +0200

    Program

--- ConsoleAppStringsTry/Program.cs
+++ ConsoleAppStringsTry/Program.cs
@@ -6,7 +6,16 @@ namespace ConsoleAppStringsTry
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //string string1 = "mississippi";
+            //int n = string1.IndexOf("is", 3);
+            //string string2 = string1.Substring(4, 2);
+            //Console.WriteLine(string2);
+
+            string name = "Bob";
+            DateTime hours = new DateTime(2011, 3, 12);
+            TimeSpan alive = DateTime.Now - hours;
+            Console.WriteLine($"Name = {name}, total days alive = {Math.Ceiling(alive.TotalDays)}");
+
         }
     }
 }
